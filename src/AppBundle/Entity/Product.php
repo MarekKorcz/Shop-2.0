dcf92aa725a9c9c262522b7fddc03991e4b4a78b
Marek Korcz <mark.korcz@gmail.com>
2017-06-20 22:28:13 +0200

    Add Product entity

---
+++ src/AppBundle/Entity/Product.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * Product
+ *
+ * @ORM\Table(name="product")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ProductRepository")
+ */
+class Product
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=30)
+     */
+    private $name;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255)
+     */
+    private $description;
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="price", type="float")
+     */
+    private $price;
+    
+    /**
+     * @ORM\ManyToOne(targetEntity="Category", inversedBy="products")
+     * @ORM\JoinColumn(name="category_id", referencedColumnName="id")
+     */
+    private $category;
+    
+    /**
+     * @ORM\OneToMany(targetEntity="Comment", mappedBy="product", cascade={"All"})
+     */
+    private $comments;
+    
+    /**
+     * @ORM\OneToMany(targetEntity="Product_Order", mappedBy="product", cascade={"All"})
+     */
+    private $productOrders;
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Product
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Product
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+        return $this;
+    }
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+    /**
+     * Set price
+     *
+     * @param float $price
+     * @return Product
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+        return $this;
+    }
+    /**
+     * Get price
+     *
+     * @return float 
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+    
+    public function setCategory($category) {
+        
+        $this->category = $category;
+        
+        return $this;
+    }
+    
+    public function getCategory(){
+        
+        return $this->category;
+    }
+    
+    public function getComments(){
+        
+        return $this->comments;
+    }
+    
+    public function getOrder(){
+        
+        return $this->productOrders;
+    }
+    
+    public function __toString(){
+        
+        return $this->getName();
+    }
+    
+    public function __construct(){
+        
+        $this->comments = new ArrayCollection();
+        $this->productOrders = new ArrayCollection();
+    }
+}
