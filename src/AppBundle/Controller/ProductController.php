33690036f67cfc8810fddf6dd64c85cd09ab004d
Marek Korcz <mark.korcz@gmail.com>
2017-08-13 21:50:04 +0200

    Add product entity + generate CRUD

---
+++ src/AppBundle/Controller/ProductController.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Product;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Product controller.
+ *
+ * @Route("product")
+ */
+class ProductController extends Controller
+{
+    /**
+     * Lists of all product entities.
+     *
+     * ADMIN
+     * 
+     * @Route("/list", name="product_list")
+     * @Method("GET")
+     */
+    public function listAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $products = $em->getRepository('AppBundle:Product')->findAll();
+
+        return $this->render('product/list.html.twig', array(
+            'products' => $products,
+        ));
+    }
+
+    /**
+     * Creates a new product entity.
+     *
+     * ADMIN
+     * 
+     * @Route("/new", name="product_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $product = new Product();
+        $form = $this->createForm('AppBundle\Form\ProductType', $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($product);
+            $em->flush();
+
+            return $this->redirectToRoute('product_show', array('nameUrl' => $product->getNameUrl()));
+        }
+
+        return $this->render('product/new.html.twig', array(
+            'product' => $product,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a product entity.
+     *
+     * @Route("/{nameUrl}", name="product_show")
+     * @Method("GET")
+     */
+    public function showAction(Product $product)
+    {
+        $deleteForm = $this->createDeleteForm($product);
+
+        return $this->render('product/show.html.twig', array(
+            'product' => $product,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing product entity.
+     *
+     * ADMIN
+     * 
+     * @Route("/{nameUrl}/edit", name="product_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Product $product)
+    {
+        $deleteForm = $this->createDeleteForm($product);
+        $editForm = $this->createForm('AppBundle\Form\ProductType', $product);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('product_edit', array('nameUrl' => $product->getNameUrl()));
+        }
+
+        return $this->render('product/edit.html.twig', array(
+            'product' => $product,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a product entity.
+     * 
+     * ADMIN
+     *
+     * @Route("/{nameUrl}", name="product_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Product $product)
+    {
+        $form = $this->createDeleteForm($product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($product);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('product_list');
+    }
+
+    /**
+     * Creates a form to delete a product entity.
+     *
+     * @param Product $product The product entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Product $product)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('product_delete', array('nameUrl' => $product->getNameUrl())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
