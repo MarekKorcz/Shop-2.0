8aa8c5e955ad17128fed904bf81df49ed79df035
Marek Korcz <mark.korcz@gmail.com>
2017-12-03 23:01:09 +0100

    Adding CartController and /cart routing

---
+++ src/AppBundle/Controller/CartController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Cart;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+
+
+/**
+ * Cart controller.
+ *
+ * @Route("cart")
+ */
+class CartController extends Controller
+{
+    /**
+     * List of products in cart
+     *
+     * @Route("/", name="cart_list")
+     * @Method("GET")
+     */
+    public function cartAction()
+    {
+        // cart logic
+        
+        return $this->render('cart/cart.html.twig');
+    }
+}
