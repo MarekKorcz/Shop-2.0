099f598c2dc036ff9e8bb63faba6c1c154f3e792
Marek Korcz <mark.korcz@gmail.com>
2018-01-08 11:17:18 +0100

    Moving from ProductController to AdmninController actions that belongs to admin

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -2,12 +2,101 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class AdminController extends Controller
 {    
+    /**
+     * Creates a new product entity.
+     * 
+     * @Route("/product/new", name="product_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $product = new Product();
+        $form = $this->createForm('AppBundle\Form\ProductType', $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($product);
+            $em->flush();
+
+            return $this->redirectToRoute('product_show', array('nameUrl' => $product->getNameUrl()));
+        }
+
+        return $this->render('product/new.html.twig', array(
+            'product' => $product,
+            'form' => $form->createView(),
+        ));
+    }
+    
+        /**
+     * Displays a form to edit an existing product entity.
+     * 
+     * @Route("/product/{nameUrl}/edit", name="product_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Product $product)
+    {
+        $deleteForm = $this->createDeleteForm($product);
+        $editForm = $this->createForm('AppBundle\Form\ProductType', $product);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('product_edit', array('nameUrl' => $product->getNameUrl()));
+        }
+
+        return $this->render('product/edit.html.twig', array(
+            'product' => $product,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+    
+    /**
+     * Deletes a product entity.
+     *
+     * @Route("/product/{nameUrl}", name="product_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Product $product)
+    {
+        $form = $this->createDeleteForm($product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($product);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('product_list');
+    }
+
+    /**
+     * Creates a form to delete a product entity.
+     *
+     * @param Product $product The product entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Product $product)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('product_delete', array('nameUrl' => $product->getNameUrl())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+    
     /**
      * Lists all users.
      *
@@ -22,6 +111,23 @@ class AdminController extends Controller
 
         return $this->render('admin/customers.html.twig', array(
             'users' => $users,
+        ));
+    }
+    
+    /**
+     * Lists of all product entities.
+     * 
+     * @Route("/product/list", name="product_list")
+     * @Method("GET")
+     */
+    public function listAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $products = $em->getRepository('AppBundle:Product')->findAll();
+
+        return $this->render('product/list.html.twig', array(
+            'products' => $products,
         ));
     }
 }
