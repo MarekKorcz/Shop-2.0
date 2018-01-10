dcc1420c2b1231774473bae0f1c999dd8d5583ad
Marek Korcz <mark.korcz@gmail.com>
2018-01-10 12:55:09 +0100

    Moving from CategoryController to AdminController, actions that belongs to admin

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Product;
+use AppBundle\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -114,6 +115,112 @@ class AdminController extends Controller
     {
         return $this->createFormBuilder()
             ->setAction($this->generateUrl('product_delete', array('nameUrl' => $product->getNameUrl())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+    
+    
+    
+    /**
+     * Creates a new category entity.
+     * 
+     * @Route("/category/new", name="category_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newCategoryAction(Request $request)
+    {
+        $category = new Category();
+        $form = $this->createForm('AppBundle\Form\CategoryType', $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($category);
+            $em->flush();
+
+            return $this->redirectToRoute('category_show', array('nameUrl' => $category->getNameUrl()));
+        }
+
+        return $this->render('category/new.html.twig', array(
+            'category' => $category,
+            'form' => $form->createView(),
+        ));
+    }
+    
+    /**
+     * Displays a form to edit an existing category entity.
+     * 
+     * @Route("/category/{nameUrl}/edit", name="category_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editCategoryAction(Request $request, Category $category)
+    {
+        $deleteForm = $this->createCategoryDeleteForm($category);
+        $editForm = $this->createForm('AppBundle\Form\CategoryType', $category);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('category_edit', array('nameUrl' => $category->getNameUrl()));
+        }
+
+        return $this->render('category/edit.html.twig', array(
+            'category' => $category,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+    
+    /**
+     * Lists all category entities.
+     *
+     * @Route("/category/list", name="category_list")
+     * @Method("GET")
+     */
+    public function categoriesListAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $categories = $em->getRepository('AppBundle:Category')->findAll();
+
+        return $this->render('category/list.html.twig', array(
+            'categories' => $categories,
+        ));
+    }
+    
+    /**
+     * Deletes a category entity.
+     * 
+     * @Route("/category/{nameUrl}", name="category_delete")
+     * @Method("DELETE")
+     */
+    public function deleteCategoryAction(Request $request, Category $category)
+    {
+        $form = $this->createCategoryDeleteForm($category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($category);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('category_list');
+    }    
+    
+    /**
+     * Creates a form to delete a category entity.
+     *
+     * @param Category $category The category entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createCategoryDeleteForm(Category $category)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('category_delete', array('nameUrl' => $category->getNameUrl())))
             ->setMethod('DELETE')
             ->getForm()
         ;
