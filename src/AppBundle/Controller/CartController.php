878a778bfd3934d1654ce4d691ec0e40d7f19154
Marek Korcz <mark.korcz@gmail.com>
2018-01-12 13:09:41 +0100

    Creating a 'crossroad' action in CartController to decide whether user is logged or not

--- src/AppBundle/Controller/CartController.php
+++ src/AppBundle/Controller/CartController.php
@@ -2,11 +2,11 @@
 
 namespace AppBundle\Controller;
 
-//use AppBundle\Entity\Cart;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 
 /**
@@ -40,5 +40,38 @@ class CartController extends Controller
         // cart summary
         
         return $this->render('cart/cart-confirm.html.twig');
+    }
+    
+    /**
+     * Add product to cart
+     * 
+     * @Route("/add/{productId}", name="cart_add")
+     * @Method({"GET", "POST"})
+     */
+    public function addProductToCart(Request $request, AuthorizationCheckerInterface $authChecker, $productId)
+    {
+        
+        if (false === $authChecker->isGranted('ROLE_ADMIN')) {
+
+            $clientIp = $request->getClientIp();
+            
+            $this->continueAsNotRegistered($productId, $clientIp);
+            
+        } elseif (true === $authChecker->isGranted('ROLE_ADMIN')) {
+
+            $userId = $this->get('security.token_storage')->getToken()->getUser()->getId();
+            
+            $this->continueAsRegistered($productId, $userId);
+        }
+    }
+    
+    private function continueAsNotRegistered($productId, $clientIp)
+    {
+        var_dump('Product id: '.$productId.', Client ip address: '.$clientIp);die;
+    }
+    
+    private function continueAsRegistered($productId, $userId)
+    {
+        var_dump('Product id: '.$productId.', User id: '.$userId);die;
     }
 }
